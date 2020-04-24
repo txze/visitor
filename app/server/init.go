6cc569a089060a0b6481dee218ad73e64c79da30
tangs <tangscna@gmail.com>
2020-04-24 19:49:38 +0800

    modify '修改redis连接配置'

--- app/server/init.go
+++ app/server/init.go
@@ -6,24 +6,22 @@ import (
 	"github.com/gomodule/redigo/redis"
 	"github.com/spf13/viper"
 	"visitor/app/server/router"
-	"visitor/client/gorm_client"
 	"visitor/client/redis_client"
 )
 
 func Init() *gin.Engine {
-	var err error
-	// 连接数据库
-	_, err = gorm_client.Dial("mysql", viper.GetString("mysql.address"))
-	if err != nil {
-		panic(err)
-	}
-
-	// 初始化表
-	err = gorm_client.Client.Master().Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4 auto_increment=1").
-		AutoMigrate().Error
-	if err != nil {
-		panic(err)
-	}
+	//// 连接数据库
+	//_, err = gorm_client.Dial("mysql", viper.GetString("mysql.address"))
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//// 初始化表
+	//err = gorm_client.Client.Master().Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4 auto_increment=1").
+	//	AutoMigrate().Error
+	//if err != nil {
+	//	panic(err)
+	//}
 	redisInit()
 	// 初始化路由
 	r := router.Init()
@@ -39,7 +37,15 @@ func redisInit() {
 		MaxActive:   0,   //连接池最大连接数量,不确定可以用0（0表示自动定义），按需分配
 		IdleTimeout: 300, //连接关闭时间 300秒 （300秒不使用自动关闭）
 		Dial: func() (redis.Conn, error) { //要连接的redis数据库
-			return redis.Dial("tcp", viper.GetString("redis.master.address"))
+			c, err := redis.Dial("tcp", viper.GetString("redis.master.address"))
+			if err != nil {
+				return nil, err
+			}
+			_, err = c.Do("AUTH", viper.GetString("redis.master.password"))
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
 		},
 	}
 
@@ -49,7 +55,15 @@ func redisInit() {
 		MaxActive:   0,   //连接池最大连接数量,不确定可以用0（0表示自动定义），按需分配
 		IdleTimeout: 300, //连接关闭时间 300秒 （300秒不使用自动关闭）
 		Dial: func() (redis.Conn, error) { //要连接的redis数据库
-			return redis.Dial("tcp", viper.GetString("redis.master.address"))
+			c, err := redis.Dial("tcp", viper.GetString("redis.master.address"))
+			if err != nil {
+				return nil, err
+			}
+			_, err = c.Do("AUTH", viper.GetString("redis.master.password"))
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
 		},
 	}
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-sql-driver/mysql v1.4.1
-	github.com/gogo/protobuf v1.2.1
 	github.com/golang/protobuf v1.3.5 // indirect
 	github.com/gomodule/redigo v2.0.1-0.20180627144507-2cd21d9966bf+incompatible
 	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
@@ -15,15 +14,15 @@ require (
 	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
 	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
-	github.com/silenceper/wechat v1.2.3
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/viper v1.6.2
 	github.com/tebeka/strftime v0.1.3 // indirect
 	github.com/txze/visitorpb v0.0.0-20200404065930-2e814885e9a8
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
 	golang.org/x/sys v0.0.0-20200327173247-9dae0f8f5775 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20200330113809-af700f360a68 // indirect
 	google.golang.org/grpc v1.28.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
