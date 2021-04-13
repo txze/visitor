f4add40fa98d30ab40fd3d208739641d03b4648a
lvxiaorun <lvxiaorun@gmail.com>
2021-04-14 00:29:14 +0800

    add:增加一个ping接口

--- app/server/router/router.go
+++ app/server/router/router.go
@@ -2,9 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Router(r *gin.Engine) *gin.Engine {
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"msg": "pong"})
+	})
 	return r
 }
